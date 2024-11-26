d373598ecbb868596e1e660789ba49680e883bd2
Reo <c0b23059f0@edu.teu.ac.jp>
2024-11-26 14:59:40 +0900

    練習4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,16 +1,33 @@
 import os
-import sys
 import random
+import sys
 import pygame as pg
 
 
-WIDTH, HEIGHT = 1100, 600
-DELTA= {pg.K_UP: (0, -5),
-        pg.K_DOWN :(0, +5),
-        pg.K_LEFT: (-5, 0),
-        pg.K_RIGHT: (+5, 0)
-        }
+WIDTH, HEIGHT = 1100, 650
+DELTA = {
+    pg.K_UP: (0,-5),
+    pg.K_DOWN: (0,+5),
+    pg.K_LEFT: (-5,0),
+    pg.K_RIGHT: (+5,0),
+}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def check_bound(rct: pg.rect) -> tuple[bool,bool]:
+    """
+    引数で与えたRectが画面の中か外かを判断する
+    引数:こうかとんのRect or 爆弾Rect 
+    戻り値：真理値タプル(横,縦)/画面内:True,画面外:False
+    """
+    yoko, tate = True,True
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+
+    return yoko,tate
+    
 
 
 def main():
@@ -20,18 +37,24 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
-    bb_img = pg.Surface((20,20)) #爆弾のsurface
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
-    bb_rct = bb_img.get_rect()
-    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
-    bb_img.set_colorkey((0, 0, 0)) #黒の透過
+    bb_img = pg.Surface((20,20))  # 爆弾surface
+    pg.draw.circle(bb_img, (255,0,0),(10,10),10)  # 爆弾円を描く
+    bb_rct = bb_img.get_rect()  # 爆弾rectの抽出
+    bb_rct.centerx = random.randint(0,WIDTH)
+    bb_rct.centery  = random.randint(0,HEIGHT)
+    bb_img.set_colorkey((0,0,0))  # 四隅の黒を透過
+
+    vx, vy = +5,-5  # 爆弾移動ベクトル
+
+
     clock = pg.time.Clock()
-    vx, vy = +5, -5
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bb_rct):
+            return  # ゲームオーバー
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
@@ -40,16 +63,21 @@ def main():
             if key_lst[key]:
                 sum_mv[0] += tpl[0]
                 sum_mv[1] += tpl[1]
-
-    
+        
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx, vy)
+        bb_rct.move_ip(vx,vy)
+        yoko,tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
-
 
 
 if __name__ == "__main__":
